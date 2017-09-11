d7e49399b11f9461f4baf3e0feb615965c2874ba
Mark <mark.aaron.fisher@gmail.com>
2017-09-11 12:32:41 -0700

    add one-way attribute binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,14 +6,20 @@ import { Component } from "@angular/core";
   <StackLayout>
     <Image src = "res://logo_login" stretch = "none" horizontalAlignment = "center"></Image>
     <TextField hint = "Email Address" keyboardType = "email"
-    autocorrect = "false" autocapitalizationType = "none"></TextField>
+    autocorrect = "false" autocapitalizationType = "none" [text]="email"></TextField>
     <TextField hint = "Password" secure = "true"></TextField>
 
-    <Button text="Sign in" class="submit-button"></Button>
+    <Button text="Sign in" class="submit-button" (tap) = "submit()"></Button>
     <Button text = "Sign up for Groceries"></Button>
   </StackLayout>
   `,
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 
-export class AppComponent {}
+export class AppComponent {
+  email = "nativescriptrocks@telerik.com";
+  submit(){
+    // console.log(`hello ${new Date()}`);
+    alert("You're using: " + this.email);
+  }
+}
